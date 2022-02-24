a8cc190815679180a404b7768929be2b5d9a6c31
sebastiand88 <damiansebastian88@gmail.com>
2022-02-24 20:56:41 +0000

    add django_meetup page

--- information/urls.py
+++ information/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-  path("", views.home, name="information_home")
+  path("", views.home, name="information_home"),
+  path("django_meetup/", views.django_meetup, name="django_meetup")
 ]
