97ac68306c9e5f6b1e8ba6af2966f9e666bb1645
sebastiand88 <damiansebastian88@gmail.com>
2022-03-03 20:46:09 +0000

    add events to db

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+  cost = models.DecimalField(max_digits=6, decimal_places=2)
+
+  def __str__(self):
+    return self.title
+
--- events/views.py
+++ events/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
 def details(request):
   return render(request, "events/details.html")
 
 def events(request):
-  return render(request, "events/events.html")
+  events = Event.objects.all()
+  return render(request, "events/events.html", {"events": events})
+
