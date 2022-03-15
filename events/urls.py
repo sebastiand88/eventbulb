08d462d045408fd077626abd1a4d161e61e0225d
sebastiand88 <damiansebastian88@gmail.com>
2022-03-15 20:55:28 +0000

    add future and past events

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path("<int:id>/", views.details, name="events_details"),
   path('<int:id>/add_attending/', views.add_attending, name="events_add_attending"),
   path('<int:id>/remove_attending/', views.remove_attending, name="events_remove_attending"),
+  path('dashboard/', views.dashboard, name="events_dashboard")
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 from .models import Event
+from accounts.models import UserProfile
 
 # Create your views here.
+def get_user_profile(request):
+  if request.user.is_authenticated:
+    [profile, created] = UserProfile.objects.get_or_create(user=request.user)
+    return profile
+
+
 def events(request):
+  today = datetime.today()
 
   filter_map = {
     "title": "title__icontains",
@@ -13,19 +22,19 @@ def events(request):
   filters = {}
 
   for key, value in request.GET.items():
-        filter_key = filter_map[key]
-        if value:
-            filters[filter_key] = value
+    filter_key = filter_map[key]
+    if value:
+      filters[filter_key] = value
 
-  events = Event.objects.filter(**filters)
+  events = Event.objects.filter(datetime__gte=today).filter(**filters).order_by("datetime")
   return render(request, "events/events.html", {"events": events})
 
 @login_required
 def add_attending(request, id):
-    event = get_object_or_404(Event, id=id)
-    if request.method == "POST":
-        request.user.profile.attending.add(event)
-    return redirect("events_list")
+  event = get_object_or_404(Event, id=id)
+  if request.method == "POST":
+    request.user.profile.attending.add(event)
+  return redirect("events_list")
 
 
 @login_required
@@ -39,4 +48,14 @@ def remove_attending(request, id):
 def details(request, id):
   eventFromDB = get_object_or_404(Event, id = id)
   return render(request, "events/details.html", {"event": eventFromDB})
+
+def dashboard(request):
+  today = datetime.today()
+
+  filters = {}
+  
+  user_profile = get_user_profile(request)
+  future_attend = user_profile.attending.filter(datetime__gte=today).filter(**filters).order_by("datetime")
+  past_attend = user_profile.attending.filter(datetime__lte=today).filter(**filters).order_by("datetime")
+  return render(request, "events/dashboard.html", {"future": future_attend, "past": past_attend})
   
